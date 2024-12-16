21c41569e26d191906c8a3b51f1dfce486653b5d
ayush <ayush.bisen@pabbly.com>
2024-12-16 21:30:35 +0530

    added List Name Handeler

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 
+import listNameReducer from './slice/listNameSlice';
 import fileUploadReducer from './slice/upload-slice';
 
 export const store = configureStore({
   reducer: {
     fileUpload: fileUploadReducer,
+    listName: listNameReducer,
   },
 });
 
